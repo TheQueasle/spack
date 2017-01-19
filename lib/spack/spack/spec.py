75bbf10fe45b60d3c4ab68bb2534cafd6a332f72
Micheal Quinn <quinnm@missouri.edu>
2017-01-19 14:44:52 -0600

    spec.py: more attempts.  This one is to test if we can write 'foobar' if all variants are FALSE

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -2511,7 +2511,10 @@ class Spec(object):
                         for k,v in self.variants.items():
                             if v.value is False:
                                 del self.variants[k]
-                        write(fmt % str(self.variants), '+')
+                        if self.variants:
+                            write(fmt % str(self.variants), '+')
+                        else:
+                            write(fmt % 'foobar', '+')
                 elif named_str == 'ARCHITECTURE':
                     if self.architecture and str(self.architecture):
                         write(fmt % str(self.architecture) + ' ', ' arch=')
